51025360ee56a93e10617269610e314d68e0030b
amado <amado.diaz.jr@gmail.com>
2015-11-20 03:17:58 -0500

    working fat jar

--- src/main/java/com/hackathon/Game.java
+++ src/main/java/com/hackathon/Game.java
@@ -3,7 +3,7 @@ package com.hackathon;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
-public class StockServiceTest {
+public class Game {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
  
